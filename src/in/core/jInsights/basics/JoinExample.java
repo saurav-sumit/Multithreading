47c4611a024bd61fb286e44daa4e271471c1eb0d
Sumit Kumar Saurav <sumitk@cdac.in>
2016-05-19 14:51:30 +0530

    "initial commit"

---
+++ src/in/core/jInsights/basics/JoinExample.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.core.jInsights.basics;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author sumit kumar saurav<sumitk@cdac.in>
+ *
+ */
+class XTask implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i);
+        }
+    }
+
+}
+
+public class JoinExample {
+
+    public static void main(String[] args) {
+        XTask xTask = new XTask();
+        Thread tA = new Thread(xTask);
+        Thread tB = new Thread(xTask);
+        tA.start();
+
+        try {
+            tA.join();
+
+        } catch (InterruptedException ex) {
+            Logger.getLogger(JoinExample.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        tB.start();
+        System.out.println(Thread.currentThread().getName() + ".... is completed");
+    }
+}
